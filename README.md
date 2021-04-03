1892f423e45ae60d3ed4ef5f4573974fd4d06f75
Lucie Trojanova <trojanova33@gmail.com>
2021-04-03 11:36:19 +0200

    final

--- README.md
+++ README.md
@@ -14,22 +14,22 @@ Na obrazovce se bude na náhodném místě objevovat mince a pokaždé, když k 
 Technický popis
 ---
 
-- Máme panáčka a nastavené absolutní pozicování (už připraveno v CSS) a umíme ho posouvat pomocí **top/left**.
-- Vytvoříme proměnné pro startovní souřadnice panáčka a umístíme ho na ně.
-- Přidáme na <body> událost **onkeydown** a budeme testovat kurzorové šipky.
-- Pro jednotlivé šipky měníme souřadnice a vždy znovu umístíme panáčka.
-- Panáček leze z obrazovky - omezíme jeho pohyb na 0 - **window.innerWidth/innerHeight**.
+✔ - Máme panáčka a nastavené absolutní pozicování (už připraveno v CSS) a umíme ho posouvat pomocí **top/left**. 
+✔ - Vytvoříme proměnné pro startovní souřadnice panáčka a umístíme ho na ně. 
+✔ - Přidáme na <body> událost **onkeydown** a budeme testovat kurzorové šipky.
+✔ - Pro jednotlivé šipky měníme souřadnice a vždy znovu umístíme panáčka.
+✔ - Panáček leze z obrazovky - omezíme jeho pohyb na 0 - **window.innerWidth/innerHeight**.
 
-- Přidáme minci (je v HTML v komentáři).
-- Na startu hry umístíme minci na náhodnou pozici.
-- Při každém pohybu testujeme, zda se panáček neprotíná s mincí - v JS připravená podmínka pro průnik dvou obdélníků.
-- Pokud panáček sebere minci, posuneme minci na jinou náhodnou pozici.
+✔ - Přidáme minci (je v HTML v komentáři).
+✔ - Na startu hry umístíme minci na náhodnou pozici.
+✔ - Při každém pohybu testujeme, zda se panáček neprotíná s mincí - v JS připravená podmínka pro průnik dvou obdélníků.
+✔ - Pokud panáček sebere minci, posuneme minci na jinou náhodnou pozici.
 
 Možná vylepšení, pro ultimátní zážitek ze hry!
 ---
 
-- V průběhu hry bude hrát zvukový podkres.
-- Při sebrání mince přehrajeme zvuk.
-- Přidáme score a při sebrání mince přičítáme.
-- Při score > 5 přehrajeme fanfáru a zobrazíme vítěznou hlášku.
-- Při posunu panáčka ho natočíme do správného směru.
+✔ - V průběhu hry bude hrát zvukový podkres.
+✔ - Při sebrání mince přehrajeme zvuk.
+✔ - Přidáme score a při sebrání mince přičítáme.
+✔ - Při score > 5 přehrajeme fanfáru a zobrazíme vítěznou hlášku.
+✔ - Při posunu panáčka ho natočíme do správného směru.
--- main.js
+++ main.js
@@ -1,9 +1,125 @@
-// toto budeš potřebovat později
-/*
-if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-	// panacek a mince se prekryvaji
+// použité proměnné
+let panacek, panacekX, panacekY, panacekSirka, panacekVyska;
+let mince, minceX, minceY, minceSirka, minceVyska;
+let score, scoreElement;
+let newScore = 0;
+
+
+//pri nacteni stranky
+function nacteniStranky() {
+	panacek = document.querySelector("#panacek");
+	mince = document.querySelector("#mince")
+
+	//sirka a vyska panacka
+	panacekSirka = panacek.width;
+	panacekVyska = panacek.height;
+
+	//panacek na stred stranky
+	panacekX = Math.round(window.innerWidth / 2 - panacekSirka / 2);
+	panacekY = Math.round(window.innerHeight / 2 - panacekVyska / 2);
+
+	//umisteni panacka na startovni pozici
+	umistiPanacka();
+
+	//sirka a vyska mince
+	minceSirka = mince.width;
+	minceVyska = mince.height;
+
+	//nahodne umisteni mince
+	novaMince();
 }
-*/
+
+//umisti panacka na jeho souradnice
+function umistiPanacka() {
+	panacek.style.left = panacekX + "px";
+	panacek.style.top = panacekY + "px";
+}
+
+//generovani nove pozice mince
+function novaMince() {
+	minceX = Math.floor(Math.random() * (window.innerWidth - minceSirka));
+	minceY = Math.floor(Math.random() * (window.innerHeight - minceVyska));
+
+	mince.style.left = minceX + "px";
+	mince.style.top =  minceY + "px";
+}
 
 
-// sem začni psát svůj program
+//pri stisku klavesy, zjistím pomoci API, vlastnost key
+function priStiskuKlavesy(udalost){
+	prehrajHudbu();
+
+	if (udalost.key ==="ArrowLeft") {
+		panacekX -= 10;
+		if (panacekX < 0) {
+			panacekX = 0;
+		}
+		panacek.src = "obrazky/panacek-vlevo.png";
+	}
+
+	if (udalost.key ==="ArrowRight") {
+		panacekX += 10;
+		if (panacekX + panacekSirka > window.innerWidth) {
+			panacekX = window.innerWidth - panacekSirka;
+		}
+		panacek.src = "obrazky/panacek-vpravo.png";
+	}
+
+	if (udalost.key ==="ArrowUp") {
+		panacekY -= 10;
+		if (panacekY < 0) {
+			panacekY = 0;
+		}
+		panacek.src = "obrazky/panacek-nahoru.png";
+	}
+
+	if (udalost.key ==="ArrowDown") {
+		panacekY += 10;
+		if (panacekY + panacekVyska > window.innerHeight) {
+			panacekY = window.innerHeight - panacekVyska;
+		}
+		panacek.src = "obrazky/panacek.png";
+	}
+
+	umistiPanacka();
+	otestujKolizi();
+};
+
+//funkce otestovani kolize panacka s minci (prunik 2 obdelniku)
+function otestujKolizi() {
+	if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
+		// panacek a mince se prekryvaji
+		prehrajMince();
+		pricteniScore();
+		novaMince();
+
+	}
+}
+
+
+//newScore = 0, definovane pred funkci, jinak se to bude stale počitat od nuly!
+function pricteniScore() {
+	scoreElement = document.querySelector("#score");
+	newScore++;
+	scoreElement.textContent = newScore;
+
+	if (newScore >= 5) {
+		prehrajFanfaru();
+		scoreElement.textContent = ("Gratuluji, vyhrál jsi!");
+	}
+}
+
+function prehrajHudbu() {
+	let hudbaElement = document.getElementById("hudba");
+	hudbaElement.play();
+}
+
+function prehrajFanfaru() {
+	let fanfaraElement = document.getElementById("zvukfanfara");
+	fanfaraElement.play();
+}
+
+function prehrajMince() {
+	let minceElement = document.getElementById("zvukmince");
+	minceElement.play();
+}
